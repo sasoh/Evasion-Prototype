e508c10f054a29f9990917b45b18d4b35e05c294
Alexander Popov <lxndrppv@gmail.com>
2026-02-06 17:39:14 +0200

    feat: animated opposition UI elements

--- Assets/Scripts/AnimationProperties.cs
+++ Assets/Scripts/AnimationProperties.cs
@@ -4,4 +4,5 @@ public static class AnimationProperties
 {
     public const float MovementTweenDuration = 0.2f;
     public const float RotationTweenDuration = 0.1f;
+    public const float UITweenDuration = 0.5f;
 }
---
+++ Assets/Scripts/OppositionUIAnimator.cs
@@ -0,0 +1,32 @@
+using Pixelplacement;
+using UnityEngine;
+
+public class OppositionUIAnimator : MonoBehaviour
+{
+    [SerializeField] private GameObject imageThinking;
+    [SerializeField] private GameObject imageSearching;
+
+    private void Start()
+    {
+        imageSearching.transform.localScale = 0.6f * Vector3.one;
+        Tween.LocalScale(
+            imageSearching.transform,
+            Vector3.one,
+            AnimationProperties.UITweenDuration,
+            0.0f,
+            Tween.EaseInOut,
+            Tween.LoopType.PingPong
+        );
+
+        const float rotation = 40.0f;
+        imageThinking.transform.Rotate(imageSearching.transform.forward, -rotation);
+        Tween.LocalRotation(
+            imageThinking.transform,
+            new Vector3(0.0f, 0.0f, rotation),
+            AnimationProperties.UITweenDuration,
+            0.0f,
+            Tween.EaseInOut,
+            Tween.LoopType.PingPong
+        );
+    }
+}
